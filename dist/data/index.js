0367417fb9005b452229219aa197b0bdb7a55d57
Alejandro Ramirez <aramirez@unphu.edu.do>
2022-07-17 15:46:51 -0400

    commit initial

--- dist/data/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Seeders_1 = require("./seeders/cli/Seeders");
-exports.define = Seeders_1.define;
-exports.factory = Seeders_1.factory;
-exports.loadFactories = Seeders_1.loadFactories;
-exports.loadSeeders = Seeders_1.loadSeeders;
-//# sourceMappingURL=index.js.map
--- dist/test/helpers/expect.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai = require("chai");
-const chaiAsPromised = require('chai-as-promised');
-const sinonChai = require('sinon-chai');
-chai.use(chaiAsPromised);
-chai.use(sinonChai);
-exports.expect = chai.expect;
-//# sourceMappingURL=expect.js.map
